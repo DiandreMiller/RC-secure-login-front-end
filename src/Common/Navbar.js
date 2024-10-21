da17e1fadfe1972c8a30401cca09b94d95890cf3
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-10-20 21:46:45 -0400

    Functionality: Added login

--- src/Common/Navbar.js
+++ src/Common/Navbar.js
@@ -1,8 +1,18 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../authenthication/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const { logout } = useAuth(); 
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout(); 
+    navigate('/');
+  };
+
   return (
-    <nav className="bg-black p-4">
+    <nav className="bg-black p-4 flex justify-between items-center">
       <ul className="flex space-x-4">
         <li>
           <Link to="/" className="text-white hover:text-gray-300">
@@ -15,6 +25,12 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+      <button
+        onClick={handleLogout}
+        className="text-white hover:text-gray-300"
+      >
+        Logout
+      </button>
     </nav>
   );
 }

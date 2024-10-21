2f19104a53f062465078eacabe9590a4b48a0b93
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-10-21 13:05:53 -0400

    Functionality: Added phone number field

--- src/Validations/validationSchema.js
+++ src/Validations/validationSchema.js
@@ -3,7 +3,6 @@ import * as Yup from 'yup';
 const specialCharacters = /[!@#$%^&*(),.?":{}|<>]/;
 
 const validationSchema = Yup.object().shape({
-
     username: Yup.string().when('isLogin', {
         is: true,
         then: Yup.string().required('Username is required'),
@@ -14,10 +13,19 @@ const validationSchema = Yup.object().shape({
         .max(32, 'Password must be at most 32 characters')
         .matches(specialCharacters, 'Password must contain at least one special character')
         .required('Password is required'),
-    dateOfBirth: Yup.date().when('isLogin',{
+    dateOfBirth: Yup.date().when('isLogin', {
         is: false, 
         then: Yup.date().required('Date of Birth is required').nullable(),
-    })
+    }),
+    phoneNumber: Yup.string().when('isLogin', {
+        is: false, 
+        then: Yup.string()
+            .required('Phone number is required')
+            .matches(/^[0-9]+$/, 'Phone number must be digits only') 
+            .min(10, 'Phone number must be at least 10 digits') 
+            .max(15, 'Phone number must be at most 15 digits'), 
+        otherwise: Yup.string().notRequired(),
+    }),
 })
 
 export default validationSchema;
